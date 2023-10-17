91fbb6e8347a4584732946bb56750429bfbe3ef6
ahmedraza07 <sujariahmadraza@gmail.com>
2023-10-17 09:17:51 +0530

    done

---
+++ practis4.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+	int a=10,b;
+	printf("Enter value of B :");
+	scanf("%d",&b);
+	(a<b)?printf("condition is true"):printf("condition is wrong");
+	
+}
---
+++ pratis.3.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+	int a=.9,b;
+	printf("Enter value of B :");
+	scanf("%d",&b);
+	printf("This is shothand oper :%d",a+=b);
+	return 0;
+}
